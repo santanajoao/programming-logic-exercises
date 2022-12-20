19724db06bcba987ce472de2b16ba2bafe08bfe4
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-12-20 18:38:09 -0300

    Strings: exercício 08 resolvido

--- Desafios/Strings/desafio-strings-08.js
+++ Desafios/Strings/desafio-strings-08.js
@@ -11,8 +11,28 @@ a = á, ã, à.
 
 */
 
+const replaceAcentos = (string) => (
+  string
+    .replace(/[àáäâã]/gi, 'a')
+    .replace(/[èéëê]/gi, 'e')
+    .replace(/[ìíïî]/gi, 'i')
+    .replace(/[òóöô]/gi, 'o')
+    .replace(/[ùúüû]/gi, 'u')
+);
+
 function countLetters(string, letter) {
-  // Desenvolva seu código nessa função
+  const lowerString = string.toLowerCase();
+  const newString = replaceAcentos(lowerString);
+  const lowerLetter = letter.toLowerCase();
+
+  let counter = 0;
+  for (let index = 0; index < newString.length; index += 1) {
+    if (newString[index] === lowerLetter) {
+      counter += 1;
+    }
+  }
+
+  return counter;
 }
 
 module.exports = countLetters;
