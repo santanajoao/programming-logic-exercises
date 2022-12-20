2137818ea16444fa7f5ae914e7faf433a8708b21
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-12-20 15:01:08 -0300

    Operadores aritiméticos: refatoração exercício 012

--- Desafios/Operadores Aritméticos/desafio-operadores-aritmeticos-012.js
+++ Desafios/Operadores Aritméticos/desafio-operadores-aritmeticos-012.js
@@ -15,17 +15,14 @@ Obs: O valor do prato tem que ter duas casas decimais após a virgula.
 
 */
 
-const format = (number) => (
-  number.toLocaleString('pt-BR', {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  })
-);
-
 function weightAndValue(priceKg, plateWeight) {
   const kgWeight = plateWeight / 1000;
   const price = kgWeight * priceKg;
-  return `O prato de ${plateWeight} gramas custa: R$ ${format(price)}`;
+  const formatedPrice = price.toLocaleString('pt-BR', {
+    minimumFractionDigits: 2, maximumFractionDigits: 2
+  });
+
+  return `O prato de ${plateWeight} gramas custa: R$ ${formatedPrice}`;
 }
 
 module.exports = weightAndValue;
